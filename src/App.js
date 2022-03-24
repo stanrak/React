0c0d7bfeb5e7987bd2e5315aa0c5297a177dd4af
dungkvfx14953 <dungkvfx14953@funix.edu.vn>
2022-03-25 00:08:36 +0700

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-class App extends Component {
 
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Chao ban</p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
