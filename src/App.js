91bba81d642bd4934ba868c3637e65e72073f310
dungkvfx14953 <dungkvfx14953@funix.edu.vn>
2022-04-04 08:23:45 +0700

    adding layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Navbar, ButtonGroup, Button } from 'reactstrap';
 import './App.css';
 import StaffList from './components/StaffListComponent';
 import { STAFFS } from './shared/staffs';
@@ -15,10 +16,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='w-100' style={{backgroundColor:'blue', color:'white'}}>
-          <p>Ứng dụng quản lý nhân sự v1.0</p>
+        <div className='bg-primary d-flex p-3 text-white align-items-center' style={{height:'3vw'}}>
+          <span>Ứng dụng quản lý nhân sự v1.0</span>
         </div>
-        <StaffList staffs={this.state.staffs} />
+        <div className='container-fluid'>
+          <div className='row justify-content-between'>
+            <div className='col-12 col-md-6 col-lg-4 d-flex align-items-center'>
+              <span>Danh sách nhân viên</span>
+            </div>
+            <div className='col-12 col-md-6 col-lg-4'>
+              <ButtonGroup>
+                <Button>1 cột</Button>
+                <Button>2 cột</Button>
+                <Button>3 cột</Button>
+                <Button>4 cột</Button>
+                <Button>6 cột</Button>
+              </ButtonGroup>
+            </div>
+          </div>
+          <StaffList staffs={this.state.staffs} />
+          <div></div>
+        </div>
       </div>
     );
   }
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+import dateFormat from 'dateformat';
 
 export default class StaffList extends Component {
   constructor(props) {
@@ -22,8 +23,8 @@ export default class StaffList extends Component {
             <CardImg top width="100%" src={staff.image} alt={staff.name} />
             <CardBody>
               <CardTitle>{staff.name}</CardTitle>
-              <CardText>Ngày sinh: {staff.doB}</CardText>
-              <CardText>Ngày vào công ty: {staff.startDate}</CardText>
+              <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+              <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
               <CardText>Phòng ban: {staff.department.name}</CardText>
               <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
               <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
