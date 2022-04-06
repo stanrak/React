f36c37ffdefa969d8e635eea06106d37a1c7c60c
dungkvfx14953 <dungkvfx14953@funix.edu.vn>
2022-04-06 21:32:47 +0700

    Fix stuffs and complete class component method

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,27 +1,8 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return (
-        <div></div>
-      );
-    }
-  }
-
   render() {
-
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
