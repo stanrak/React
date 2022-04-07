8c26586f8e38e90da282b1ec9fcc3c8273a61035
dungkvfx14953 <dungkvfx14953@funix.edu.vn>
2022-04-07 16:51:11 +0700

    Change to Lab05_4 Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+import { Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
   return (
@@ -37,16 +38,26 @@ function RenderComment({comments}) {
 const DishDetail = (props) => {
   if (props.dish != null) {
     return (
-    <div className="container">
-      <div className='row'>
-        <div className="col-12 col-md-5 m-1">
-          <RenderDish dish={props.dish} />
+      <div className="container">
+        <div className='row'>
+          <Breadcrumb>
+            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className='col-12'>
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
         </div>
-        <div className="col-12 col-md-5 m-1">
-          <RenderComment comments={props.dish.comments} />
+        <div className='row'>
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComment comments={props.comments} />
+          </div>
         </div>
       </div>
-    </div>
     );
   } else {return(<div></div>)}
 }
