7a8acd7919701fc7e7a9641c534511252b319e31
dungkvfx14953 <dungkvfx14953@funix.edu.vn>
2022-04-20 16:58:50 +0700

    fix texts

--- src/components/StaffComponent.js
+++ src/components/StaffComponent.js
@@ -29,7 +29,7 @@ class RenderStaff extends Component {
             <p>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
             <p>Phòng ban: {staff.department.name}</p>
             <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
-            <p>Số ngày đã làm thêm: {staff.overTime}</p>
+            <p>Số giờ đã làm thêm: {staff.overTime}</p>
             <p>Bậc lương: {staff.salaryScale}</p>
           </div>
         </div>
